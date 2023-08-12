45cc350807255af1254157cd81bad1dbdb62e388
vcpavan <vcpavan@gmail.com>
2023-08-12 18:04:28 +0000

    Updated deploy_script.sh

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copy new files to the Nginx web server directory
-sudo cp -r /path/to/your/repo/* /var/www/html/
+sudo cp -r $(Build.ArtifactStagingDirectory) /var/www/html/
 
 # Restart Nginx
 sudo systemctl restart nginx
